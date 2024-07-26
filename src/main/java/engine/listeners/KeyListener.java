fb5d109307c5fc12618a96a9321bdd424cacd32f
Ivo Afonso Bispo <ivoafobispo@gmail.com>
2024-07-26 12:25:56 +0100

    feat: Kayboard Listener

---
+++ src/main/java/engine/listeners/KeyListener.java
@@ -0,0 +1,36 @@
+package engine.listeners;
+
+import static org.lwjgl.glfw.GLFW.GLFW_PRESS;
+import static org.lwjgl.glfw.GLFW.GLFW_RELEASE;
+
+public class KeyListener {
+
+    public static final int NUMBER_OF_GLFW_KEYBINDINGS = 350;
+
+    private static KeyListener instance;
+    private boolean[] keyPressed = new boolean[NUMBER_OF_GLFW_KEYBINDINGS];
+
+    private KeyListener() {
+
+    }
+
+    public static KeyListener get() {
+        if (KeyListener.instance == null) {
+            KeyListener.instance = new KeyListener();
+        }
+
+        return KeyListener.instance;
+    }
+
+    public static void keyCallback(long window, int key, int scancode, int action, int mods) {
+        if (action == GLFW_PRESS) {
+            get().keyPressed[key] = true;
+        } else if (action == GLFW_RELEASE) {
+            get().keyPressed[key] = false;
+        }
+    }
+
+    public static boolean isKeyPressed(int keyCode) {
+        return get().keyPressed[keyCode];
+    }
+}
