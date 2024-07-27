9b5cb0aa8e173aa8942f22cef8b7a8c0f4384906
Ivo Afonso Bispo <ivoafobispo@gmail.com>
2024-07-27 08:49:25 +0100

    feat: Create Level Scene

--- src/main/java/engine/scenes/LevelScene.java
+++ src/main/java/engine/scenes/LevelScene.java
@@ -1,4 +1,16 @@
 package engine.scenes;
 
-public class LevelScene {
+import engine.Window;
+import engine.managers.Scene;
+
+public class LevelScene extends Scene {
+
+    public LevelScene() {
+        System.out.println("Inside Level Scene");
+    }
+
+    @Override
+    public void update(float deltaTime) {
+
+    }
 }
