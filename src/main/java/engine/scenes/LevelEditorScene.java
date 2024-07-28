c0358ea45915d3910fa8082639604d53a7c8db46
Ivo Afonso Bispo <ivoafobispo@gmail.com>
2024-07-28 22:36:37 +0100

    chore: Remove unnecessary variables of shader

--- src/main/java/engine/scenes/LevelEditorScene.java
+++ src/main/java/engine/scenes/LevelEditorScene.java
@@ -13,28 +13,6 @@ import static org.lwjgl.opengl.GL30.glGenVertexArrays;
 
 public class LevelEditorScene extends Scene {
 
-    private String vertexShaderSrc = "#version 330 core\n" +
-            "\n" +
-            "layout (location = 0) in vec3 aPos;\n" +
-            "layout (location = 1) in vec4 aColor;\n" +
-            "\n" +
-            "out vec4 fColor;\n" +
-            "\n" +
-            "void main() {\n" +
-            "    fColor = aColor;\n" +
-            "    gl_Position = vec4(aPos, 1.0);\n" +
-            "}";
-
-    private String fragmentShaderSrc = "#version 330 core\n" +
-            "\n" +
-            "in vec4 fColor;\n" +
-            "\n" +
-            "out vec4 color;\n" +
-            "\n" +
-            "void main() {\n" +
-            "    color = fColor;\n" +
-            "}\n";
-
     private int vertexID;
     private int fragmentID;
     private int shaderProgram;
